47764df38a3f3c2b8a6207215219422fb5112ff6
Fei Liu <LiuF29@cardiff.ac.uk>
2021-11-18 14:27:06 +0000

    database created

---
+++ templates/HighSheriff.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS Donators;
+DROP TABLE IF EXISTS Applicants;
+
+CREATE TABLE IF NOT EXISTS `Donators` (
+  `ID`		INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `firstName`	TEXT NOT NULL,
+  `lastName`	TEXT NOT NULL,
+  `Amount`  numeric
+);
+
+-- Look at the quotes They are different on each line.  See https://sqlite.org/lang_keywords.html
+INSERT INTO 'Donators'('firstName','lastName', 'Amount' ) VALUES ('Ian','Cooper',5000);
+INSERT INTO 'Donators'('firstName','lastName', 'Amount' ) VALUES ('Fei','Liu',1000);
+
+CREATE TABLE IF NOT EXISTS `Applicants` (
+  `ID`		INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `firstName`	TEXT NOT NULL,
+  `lastName`	TEXT NOT NULL,
+  `Amount`  numeric
+);
+
+-- Look at the quotes They are different on each line.  See https://sqlite.org/lang_keywords.html
+INSERT INTO 'Applicants'('firstName','lastName', 'Amount' ) VALUES ('Ian','Cooper',5000);
+INSERT INTO 'Applicants'('firstName','lastName', 'Amount' ) VALUES ('Fei','Liu',1000);
