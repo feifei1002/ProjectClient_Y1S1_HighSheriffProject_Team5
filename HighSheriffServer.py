e2a3260eee6ab8e0160f4488c2e2888b686db064
Fei Liu <LiuF29@cardiff.ac.uk>
2021-11-19 11:05:27 +0000

    login page and database complete

--- HighSheriffServer.py
+++ HighSheriffServer.py
@@ -15,27 +15,34 @@ def returnFirst():
 
 @app.route("/adminDonators", methods = ['GET', 'POST'])
 def adminDonators():
+	# error = None
+    # if request.method == 'POST':
+    #     if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+    #         error = 'Invalid Credentials. Please try again.'
+    #     else:
+    #         return render_template('moderator.html', variable=quiz_questions[1], num_quest = numNext + 1, list_var = quiz_questions, answer_var = quiz_answers, answervariable=quiz_answers[1])
+    # return render_template('login.html', error=error)
+
 	if request.method =='GET':
 		return render_template('adminDonators.html')
+	error = None
 	if request.method =='POST':
-		try:
-			surname = request.form.get('surname', default="Error")
-			print(surname)
-			conn = sqlite3.connect(DATABASE)
-			cur = conn.cursor()
-			cur.execute("SELECT * FROM 'Donators' WHERE surname=?;", [surname])
-			data = cur.fetchall()
-			print(data)
-		except Exception as e:
-			print('there was an error')
-			print(e)
-			conn.close()
-			# return "error in adminDonators"
-			data=""
-		finally:
-			conn.close()
-			return render_template('ListDonators.html', data = data)
-
-
+		if request.form['username']!= 'admin' or request.form['password'] != 'admin':
+			error: "Wrong username or password. Please try again."
+		else:
+			try:
+				conn = sqlite3.connect(DATABASE)
+				cur = conn.cursor()
+				cur.execute("SELECT * FROM 'Donators'")
+				data = cur.fetchall()
+				print(data)
+			except Exception as e:
+				print('there was an error')
+				print(e)
+				conn.close()
+				data=""
+			finally:
+				conn.close()
+				return render_template('ListDonators.html', data = data)
 if __name__ == "__main__":
 	app.run(debug=True)
