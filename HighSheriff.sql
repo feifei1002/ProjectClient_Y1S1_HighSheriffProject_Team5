f7e1f1bddf53ff3489473119ce67651d1d4b22be
Fei Liu <liuf29@cardiff.ac.uk>
2021-11-25 14:55:03 +0000

    Revert "Merge branch 'user_story_13'"
    This reverts merge request !19

--- HighSheriff.sql
+++ HighSheriff.sql
@@ -6,12 +6,13 @@ CREATE TABLE IF NOT EXISTS `Donators` (
   `firstName`	TEXT NOT NULL,
   `surName`	TEXT NOT NULL,
   `Amount`  numeric,
-  `Email` TEXT NOT NULL
+  `Email` TEXT NOT NULL,
+  `Reason` TEXT NOT NULL
 );
 
-INSERT INTO 'Donators'('firstName','surName', 'Amount', 'Email') VALUES ('Fei','Liu',5000, 'LiuF29@cardiff.ac.uk');
-INSERT INTO 'Donators'('firstName','surName', 'Amount', 'Email') VALUES ('Peter','Hayward',4000, 'haywardpj@cardiff.ac.uk');
-INSERT INTO 'Donators'('firstName','surName', 'Amount', 'Email') VALUES ('Jashan','Vigneswaran',3000, 'vigneswaranj@cardiff.ac.uk');
+INSERT INTO 'Donators'('firstName','surName', 'Amount', 'Email', 'Reason' ) VALUES ('Fei','Liu',5000, 'LiuF29@cardiff.ac.uk', 'bla bla bla');
+INSERT INTO 'Donators'('firstName','surName', 'Amount', 'Email', 'Reason' ) VALUES ('Peter','Hayward',4000, 'haywardpj@cardiff.ac.uk', 'bla bla bla');
+INSERT INTO 'Donators'('firstName','surName', 'Amount', 'Email', 'Reason' ) VALUES ('Jashan','Vigneswaran',3000, 'vigneswaranj@cardiff.ac.uk', 'bla bla bla');
 
 
 CREATE TABLE IF NOT EXISTS `Applicants` (
@@ -26,5 +27,3 @@ CREATE TABLE IF NOT EXISTS `Applicants` (
 INSERT INTO 'Applicants'('firstName','surName', 'Amount', 'Email', 'Reason' ) VALUES ('Fei','Liu',5000, 'LiuF29@cardiff.ac.uk', 'bla bla bla');
 INSERT INTO 'Applicants'('firstName','surName', 'Amount', 'Email', 'Reason' ) VALUES ('Gianfranco','Cicciomessere',2000, 'cicciomessereg@cardiff.ac.uk', 'bla bla bla');
 INSERT INTO 'Applicants'('firstName','surName', 'Amount', 'Email', 'Reason' ) VALUES ('Stevens','Matthew',1000, 'stevensm7@cardiff.ac.uk', 'bla bla bla');
-INSERT INTO 'Applicants'('firstName','surName', 'Amount', 'Email', 'Reason') VALUES ('Peter','Hayward',4000, 'haywardpj@cardiff.ac.uk', 'hello hello hello');
-INSERT INTO 'Applicants'('firstName','surName', 'Amount', 'Email', 'Reason') VALUES ('Jashan','Vigneswaran',3000, 'vigneswaranj@cardiff.ac.uk', 'hello hello hello');
